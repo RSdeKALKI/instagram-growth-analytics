0d96a05885e5bef2ef6940c6a3e889ec056111b5
Kalki S <kalki3839@gmail.com>
2026-09-14 16:40:05 +0530

    user_segmentation.sql

--- analysis/user_segmentation.sql
+++ analysis/user_segmentation.sql
@@ -1,125 +1,140 @@
-USE instagram;
+USE Instagram;
 
--- =====================================================
--- USER SEGMENTATION
--- =====================================================
+-- User Segmentation
 
 -- 1. Segment users by content creation
 SELECT
-    u.ID,
-    u.User_name,
-    COUNT(p.ID) AS posts,
+    Users.ID,
+    Users.User_name,
+    COUNT(Photos.ID) AS posts,
     CASE
-        WHEN COUNT(p.ID) = 0 THEN 'Consumer'
-        WHEN COUNT(p.ID) BETWEEN 1 AND 5 THEN 'Light Creator'
-        WHEN COUNT(p.ID) BETWEEN 6 AND 10 THEN 'Regular Creator'
+        WHEN COUNT(Photos.ID) = 0 THEN 'Consumer'
+        WHEN COUNT(Photos.ID) BETWEEN 1 AND 5 THEN 'Light Creator'
+        WHEN COUNT(Photos.ID) BETWEEN 6 AND 10 THEN 'Regular Creator'
         ELSE 'Power Creator'
     END AS creator_segment
-FROM Users u
-LEFT JOIN Photos p
-    ON u.ID = p.User_ID
-GROUP BY u.ID, u.User_name
+FROM Users
+LEFT JOIN Photos
+    ON Users.ID = Photos.User_ID
+GROUP BY Users.ID, Users.User_name
 ORDER BY posts DESC;
-
 
 -- 2. Segment users by likes given
 SELECT
-    u.ID,
-    u.User_name,
-    COUNT(l.Photo_id) AS likes_given,
+    Users.ID,
+    Users.User_name,
+    COUNT(Likes.Photo_id) AS likes_given,
     CASE
-        WHEN COUNT(l.Photo_id) = 0 THEN 'Inactive'
-        WHEN COUNT(l.Photo_id) BETWEEN 1 AND 10 THEN 'Low Engagement'
-        WHEN COUNT(l.Photo_id) BETWEEN 11 AND 50 THEN 'Moderate Engagement'
+        WHEN COUNT(Likes.Photo_id) = 0 THEN 'Inactive'
+        WHEN COUNT(Likes.Photo_id) BETWEEN 1 AND 10 THEN 'Low Engagement'
+        WHEN COUNT(Likes.Photo_id) BETWEEN 11 AND 50 THEN 'Moderate Engagement'
         ELSE 'High Engagement'
     END AS engagement_segment
-FROM Users u
-LEFT JOIN Likes l
-    ON u.ID = l.User_id
-GROUP BY u.ID, u.User_name
+FROM Users
+LEFT JOIN Likes
+    ON Users.ID = Likes.User_id
+GROUP BY Users.ID, Users.User_name
 ORDER BY likes_given DESC;
 
-
--- 3. Combined behavioral segmentation
+-- 3. Behavioral segmentation using posts, likes given and comments made
+WITH user_activity AS (
+    SELECT
+        Users.ID,
+        Users.User_name,
+        COALESCE(posts.posts, 0) AS posts,
+        COALESCE(likes.likes_given, 0) AS likes_given,
+        COALESCE(comments.comments_made, 0) AS comments_made
+    FROM Users
+    LEFT JOIN (
+        SELECT User_ID, COUNT(*) AS posts
+        FROM Photos
+        GROUP BY User_ID
+    ) AS posts
+        ON Users.ID = posts.User_ID
+    LEFT JOIN (
+        SELECT User_id, COUNT(*) AS likes_given
+        FROM Likes
+        GROUP BY User_id
+    ) AS likes
+        ON Users.ID = likes.User_id
+    LEFT JOIN (
+        SELECT User_id, COUNT(*) AS comments_made
+        FROM Comments
+        GROUP BY User_id
+    ) AS comments
+        ON Users.ID = comments.User_id
+)
 SELECT
-    u.ID,
-    u.User_name,
-    COUNT(DISTINCT p.ID) AS posts,
-    COUNT(DISTINCT l.Photo_id) AS likes_given,
-    COUNT(DISTINCT c.ID) AS comments_made,
-    COUNT(DISTINCT f.Followee_id) AS following,
+    ID,
+    User_name,
+    posts,
+    likes_given,
+    comments_made,
     CASE
-        WHEN COUNT(DISTINCT p.ID) = 0
-             AND COUNT(DISTINCT l.Photo_id) = 0
-             AND COUNT(DISTINCT c.ID) = 0
+        WHEN posts = 0
+             AND likes_given = 0
+             AND comments_made = 0
             THEN 'Inactive'
-            
-        WHEN COUNT(DISTINCT p.ID) > 0
-             AND (
-                 COUNT(DISTINCT l.Photo_id)
-                 + COUNT(DISTINCT c.ID)
-             ) > 0
+        WHEN posts > 0
+             AND likes_given + comments_made > 0
             THEN 'Creator & Engager'
-            
-        WHEN COUNT(DISTINCT p.ID) > 0
+        WHEN posts > 0
             THEN 'Creator'
-            
-        WHEN (
-            COUNT(DISTINCT l.Photo_id)
-            + COUNT(DISTINCT c.ID)
-        ) > 0
+        WHEN likes_given + comments_made > 0
             THEN 'Consumer & Engager'
-            
         ELSE 'Consumer'
     END AS user_segment
-FROM Users u
-LEFT JOIN Photos p
-    ON u.ID = p.User_ID
-LEFT JOIN Likes l
-    ON u.ID = l.User_id
-LEFT JOIN Comments c
-    ON u.ID = c.User_id
-LEFT JOIN Follows f
-    ON u.ID = f.Follower_id
-GROUP BY u.ID, u.User_name
-ORDER BY u.ID;
+FROM user_activity
+ORDER BY ID;
 
-
--- 4. Size of each user segment
-SELECT
-    user_segment,
-    COUNT(*) AS users
-FROM (
+-- 4. Number of users in each behavioral segment
+WITH user_activity AS (
     SELECT
-        u.ID,
+        Users.ID,
+        COALESCE(posts.posts, 0) AS posts,
+        COALESCE(likes.likes_given, 0) AS likes_given,
+        COALESCE(comments.comments_made, 0) AS comments_made
+    FROM Users
+    LEFT JOIN (
+        SELECT User_ID, COUNT(*) AS posts
+        FROM Photos
+        GROUP BY User_ID
+    ) AS posts
+        ON Users.ID = posts.User_ID
+    LEFT JOIN (
+        SELECT User_id, COUNT(*) AS likes_given
+        FROM Likes
+        GROUP BY User_id
+    ) AS likes
+        ON Users.ID = likes.User_id
+    LEFT JOIN (
+        SELECT User_id, COUNT(*) AS comments_made
+        FROM Comments
+        GROUP BY User_id
+    ) AS comments
+        ON Users.ID = comments.User_id
+),
+segmented_users AS (
+    SELECT
         CASE
-            WHEN COUNT(DISTINCT p.ID) = 0
-                 AND COUNT(DISTINCT l.Photo_id) = 0
-                 AND COUNT(DISTINCT c.ID) = 0
+            WHEN posts = 0
+                 AND likes_given = 0
+                 AND comments_made = 0
                 THEN 'Inactive'
-            WHEN COUNT(DISTINCT p.ID) > 0
-                 AND (
-                     COUNT(DISTINCT l.Photo_id)
-                     + COUNT(DISTINCT c.ID)
-                 ) > 0
+            WHEN posts > 0
+                 AND likes_given + comments_made > 0
                 THEN 'Creator & Engager'
-            WHEN COUNT(DISTINCT p.ID) > 0
+            WHEN posts > 0
                 THEN 'Creator'
-            WHEN (
-                COUNT(DISTINCT l.Photo_id)
-                + COUNT(DISTINCT c.ID)
-            ) > 0
+            WHEN likes_given + comments_made > 0
                 THEN 'Consumer & Engager'
             ELSE 'Consumer'
         END AS user_segment
-    FROM Users u
-    LEFT JOIN Photos p
-        ON u.ID = p.User_ID
-    LEFT JOIN Likes l
-        ON u.ID = l.User_id
-    LEFT JOIN Comments c
-        ON u.ID = c.User_id
-    GROUP BY u.ID
-) x
+    FROM user_activity
+)
+SELECT
+    user_segment,
+    COUNT(*) AS users
+FROM segmented_users
 GROUP BY user_segment
 ORDER BY users DESC;
