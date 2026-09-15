8008ec3e6bcc5cf51489ffe4f9bcf6bdc0493361
Kalki S <kalki3839@gmail.com>
2026-09-15 21:57:25 +0530

    creator_performance.sql

--- 3. Analysis/creator_performance.sql
+++ 3. Analysis/creator_performance.sql
@@ -2,19 +2,7 @@ USE Instagram;
 
 -- Creator & Content Performance
 
--- 1. Top creators by number of posts
-SELECT
-    Users.ID,
-    Users.User_name,
-    COUNT(Photos.ID) AS total_posts
-FROM Users
-JOIN Photos
-    ON Users.ID = Photos.User_ID
-GROUP BY Users.ID, Users.User_name
-ORDER BY total_posts DESC
-LIMIT 10;
-
--- 2. Creator performance: posts, likes received and comments received
+-- 1. Which creators generate the highest total engagement?
 SELECT
     Users.ID,
     Users.User_name,
@@ -51,7 +39,7 @@ GROUP BY
     comments.comments_received
 ORDER BY likes_received DESC;
 
--- 3. Average engagement per post by creator
+-- 2. Which creators generate the highest average engagement per post?
 SELECT
     Users.ID,
     Users.User_name,
@@ -93,38 +81,4 @@ GROUP BY
     comments.comments_received
 ORDER BY avg_engagement_per_post DESC;
 
--- 4. Top 10 photos by likes
-SELECT
-    Users.User_name,
-    Photos.ID AS photo_id,
-    Photos.Image_url,
-    COUNT(Likes.User_id) AS likes
-FROM Photos
-JOIN Likes
-    ON Photos.ID = Likes.Photo_id
-JOIN Users
-    ON Users.ID = Photos.User_ID
-GROUP BY
-    Photos.ID,
-    Users.User_name,
-    Photos.Image_url
-ORDER BY likes DESC
-LIMIT 10;
 
--- 5. Top 10 photos by comments
-SELECT
-    Users.User_name,
-    Photos.ID AS photo_id,
-    Photos.Image_url,
-    COUNT(Comments.ID) AS comments
-FROM Photos
-JOIN Comments
-    ON Photos.ID = Comments.Photo_id
-JOIN Users
-    ON Users.ID = Photos.User_ID
-GROUP BY
-    Photos.ID,
-    Users.User_name,
-    Photos.Image_url
-ORDER BY comments DESC
-LIMIT 10;
